cb4a76be765f7dcf76059fd079a62d7471d0b2d0
Sotatek-NhatNguyen2 <nhat.nguyen2@sotatek.com>
2022-08-28 23:59:42 +0700

    path static

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const db = require("./db");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const port = 3000;
+const path = require("path");
 
 // connect db
 db.connect();
@@ -17,6 +18,14 @@ app.use("/api/v1/register", routes.routeRegister);
 app.use("/api/v1/login", routes.routeLogin);
 app.use("/api/v1/account", routes.routeAccount);
 app.use("/", routes.routeHome);
+
+// static path
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+app.get("/public", (req, res, next) => {
+    const pathFile = path.join(__dirname, "/index.html");
+    res.sendFile(pathFile);
+});
 
 // listen app
 app.listen(port, () => {
