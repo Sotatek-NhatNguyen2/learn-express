60a7b3515f6dbecad7ce4e8ff3b1e03a0d644d4e
Sotatek-NhatNguyen2 <nhat.nguyen2@sotatek.com>
2022-09-07 14:24:59 +0700

    cors

--- public/main.js
+++ public/main.js
@@ -4,25 +4,11 @@ let total = null
 const content = document.querySelector(".body_table");
 const pagination = document.querySelector(".pagination");
 
-axios.get(`http://localhost:3000/api/v1/account`) 
-    .then(data => data.data)
-    .then(data => total = data.total)
-    .then(() => {
-        numPage = Math.ceil(total / 2)
-        const arr = Array.from(Array(numPage).keys())
-        const html = arr.map((item, index) => (
-            `<li class="page-item">
-                <span onclick=clickPage(item + 1)" class="page-link">${item + 1}</span>
-            </li>`
-        ));
-        pagination.innerHTML = html.join('')
-    })
-
 const getData = async (asset, limit) => {
-    const data = await axios.get(`http://localhost:3000/api/v1/account?asset=${asset}&limit=${limit}`);
-    const html = data.data.map((item, index) => (
+    const res = await axios.get(`http://localhost:3000/api/v1/account?asset=${asset}&limit=${limit}`);
+    const html = res.data.data.map((item, index) => (
         `<tr>
-            <th scope="row">${index}</th>
+            <th scope="row">${index + 1}</th>
             <td>${item.username}</td>
             <td>${item.password}</td>
             <td>${item.role}</td>
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const cors = require('cors')
+
 const db = require("./db");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const port = 3000;
-const path = require("path");
 
 // connect db
 db.connect();
@@ -13,14 +15,17 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// static path
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// use cors
+app.use(cors())
+
 // routes
 app.use("/api/v1/register", routes.routeRegister);
 app.use("/api/v1/login", routes.routeLogin);
 app.use("/api/v1/account", routes.routeAccount);
 app.use("/", routes.routeHome);
-
-// static path
-app.use("/public", express.static(path.join(__dirname, "/public")));
 
 app.get("/public", (req, res, next) => {
     const pathFile = path.join(__dirname, "/public/index.html");
